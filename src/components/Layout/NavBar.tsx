9bfb2380816c20cafb2d87231fb34e05d6f0b3f4
pooyan <thepooyan@gmail.com>
2025-01-31 19:56:35 +0330

    drived user signal

--- src/components/Layout/NavBar.tsx
+++ src/components/Layout/NavBar.tsx
@@ -2,17 +2,24 @@ import pb from "../../utility/backend";
 import Login from "../Login";
 import Logout from "../Logout";
 import { isLoggedIn } from "../../utility/signal";
+import { user } from "../../utility/interface";
 
 const NavBar = () => {
 
+  let user = () => {
+    if (!isLoggedIn()) return null
+    return pb.authStore.record as user
+  }
+
   return <div class="bg-gray-200 border-b-gray-500 border-b-2 p-2 flex justify-end">
-    {isLoggedIn() && <div class="mr-auto flex flex-col">
+    {user() && <div class="mr-auto flex flex-col">
       <span>
-        {pb.authStore.record?.name}
+        {user()?.name}
       </span>
       <span>
-        {pb.authStore.record?.email}
+        {user()?.email}
       </span>
+      <img src={user()?.avatar} alt="user avatar"/>
     </div>}
     {isLoggedIn() && <Logout/> || <Login />}
   </div>;

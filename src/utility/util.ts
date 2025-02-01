117092202fffc5eac1d9094e1ff25f298113234a
pooyan <thepooyan@gmail.com>
2025-02-01 18:19:46 +0330

    tie isMyPost() to isLoggedIn() to make it practically a signal

--- src/utility/util.ts
+++ src/utility/util.ts
@@ -1,6 +1,8 @@
 import pb from "./backend"
 import { post } from "./interface"
+import { isLoggedIn } from "./signal"
 
 export const isMyPost = (post: post) => {
+  isLoggedIn()
   return pb.authStore.record?.username === post.expand?.user.username
 }
